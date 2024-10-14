f4beb8e9d84cc690b8ecd67c23c28118f071b2e8
Paduchuri Manideep <paduchurimanideep@gmail.com>
2024-10-14 14:52:25 +0530

    LEN-7 Main Terraform - Var Demo

---
+++ variables/diff_types/main.tf
@@ -0,0 +1,17 @@
+#Sample Pet Name - Using Length & Prefix as variables
+resource "random_pet" sample_pet{
+  length = var.pet_words_count
+  prefix = tolist(var.salutation)[0]
+}
+
+#Sample Fruit - Generate using list shuffler
+resource "random_shuffle" fruit{
+  input = var.fruits
+  result_count = 1
+}
+
+#Sample file - Generate file name & content using variables
+resource "local_file" file_res{
+  filename = var.file_params["filename"]
+  content = var.file_params["content"]
+}
