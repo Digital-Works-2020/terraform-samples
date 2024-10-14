53478c6ccac36fdc8e933b43fab6debe7bb66278
Paduchuri Manideep <paduchurimanideep@gmail.com>
2024-10-14 16:01:54 +0530

    LEN-7 Fix Indent

--- variables/diff_types/variables.tf
+++ variables/diff_types/variables.tf
@@ -1,28 +1,28 @@
 
 #Type is optional and used to enforce data type constraints
 variable image_id{
-   type = string
+   type    = string
    default = "ami-0c55b159cbfafe1f0"
 }
 
 variable pet_words_count{
-   type = number
+   type    = number
    default = 3
 }
 
 variable human_or_bot{
-   type = bool
+   type    = bool
    default = false
 }
 
 variable fruits{
-   type = list(string)
+   type    = list(string)
    default = ["Apple", "Orange", "Kiwi"]
 }
 
 #Set - List without duplicates
 variable salutation{
-   type = set(string)
+   type    = set(string)
    default = ["Mr", "Mrs"]
 }
 
@@ -38,18 +38,18 @@ variable file_params{
 variable cat{
   type =  object({
      name = string
-     age = number
+     age  = number
      food = list(string)
   })
   default  = {
      name = "Tom"
-     age = 21
+     age  = 21
      food = ["Fish","Cheese"]
   }
 }
 
 #Heterogenous List
 variable tuple{
-   type = tuple([string,number])
+   type    = tuple([string,number])
    default = ["Jerry",21]
 }
